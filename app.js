35929a72c672a065e2039a113c93bc76ca019d68
Sunny Pepple <slpepple01@gmail.com>
2024-07-04 14:49:55 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+ 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, yes!',
+  })
+})
+ 
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+})
